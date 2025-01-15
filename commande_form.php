5f2a70dab35a910a17f393c42427c2c9803758e6
P-S.Cypre <pscypre@gmail.com>
2025-01-15 12:51:54 -0500

    ajustement du html/css

--- commande_form.php
+++ commande_form.php
@@ -12,19 +12,26 @@
             <div class="row">
                 <div class="col-25">
                 <label for="nom_client">Nom du client:</label>
-            </div>
+                </div>
             <div class="col-75">
                 <input type="text" id="nom_client" name="nom_client" required>
             </div>
             </div>
 
-            <div class="form-group">
-                <label for="produit">Produit:</label>
+            <div class="row">
+                <div class="col-25">
+                    <label for="produit">Produit:</label>
+                </div>
+            <div class="col-75">
                 <input type="text" id="produit" name="produit" required>
+                </div>
             </div>
 
-            <div class="form-group">
+            <div class="row">
+            <div class="col-25">
                 <label for="categorie">Catégorie:</label>
+                </div>
+                <div class="col-75">
                 <select id="categorie" name="categorie" required>
                     <option value="">Sélectionnez une catégorie</option>
                     <option value="noAlcWine">Vins non alcoolisés</option>
@@ -33,19 +40,30 @@
                     <option value="newElectricCars">Voitures électriques neuves</option>
                     <option value="others">Autres</option>
                 </select>
-            </div>
+                </div>
+                </div>
+            
 
-            <div class="form-group">
+                <div class="row">
+                <div class="col-25">
                 <label for="quantite">Quantité:</label>
+                </div>
+                <div class="col-75">
                 <input type="number" id="quantite" name="quantite" min="1" required>
-            </div>
+                </div>
+                </div>
 
-            <div class="form-group">
+                <div class="row">
+                <div class="col-25">
                 <label for="date_livraison">Date de livraison:</label>
+                </div>
                 <input type="date" id="date_livraison" name="date_livraison" required>
+                </div>
+                </div>
+                <br>
+            <div class="row">
+            <input type="submit" value="Submit">
             </div>
-
-            <button type="submit">Calculer la commande</button>
         </form>    
     </div>
 </body>
---
+++ traitement_commande.php
@@ -0,0 +1,8 @@
+<?php
+// Constantes pour les taxes
+define('TAXE_FEDERALE', 0.05);
+define('TAXE_PROVINCIALE', 0.0995);
+define('PRIX_UNITAIRE', 20.00);
+
+// Validation des donneés
+?>
